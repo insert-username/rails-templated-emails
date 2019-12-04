f9207f26e5f2a9c317ed9d8ae761c987fe7f3b2f
Luke Dobinson <luke.a.dobinson@gmail.com>
2019-12-04 21:12:32 +0000

    Created TemplatedEmailController. This controller will manage the creation and sending of email templates.

---
+++ app/controllers/templated_emails_controller.rb
@@ -0,0 +1,8 @@
+class TemplatedEmailsController < ApplicationController
+
+  def index
+    #todo: list all sent emails
+    # todo: pagination?
+  end
+
+end
---
+++ app/helpers/templated_emails_helper.rb
@@ -0,0 +1,2 @@
+module TemplatedEmailsHelper
+end
