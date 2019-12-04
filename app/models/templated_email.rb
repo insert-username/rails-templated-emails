25edf1f9b7a5428597ffd6b137445be07648145c
Luke Dobinson <luke.a.dobinson@gmail.com>
2019-12-04 22:51:08 +0000

    Added some validation tests for TemplatedEmail.

--- app/models/templated_email.rb
+++ app/models/templated_email.rb
@@ -1,3 +1,6 @@
 class TemplatedEmail < ApplicationRecord
   belongs_to :message_template
+
+  validates :sender_name, presence: true
+  validates :destination_email, presence: true
 end
--- test/models/templated_email_test.rb
+++ test/models/templated_email_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class TemplatedEmailTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should save if valid" do
+    templated_email = create_valid_templated_email
+
+    assert templated_email.save
+  end
+
+  test "should have sender_name before save" do
+    templated_email = create_valid_templated_email
+    templated_email.sender_name = nil
+
+    assert_not templated_email.save
+  end
+
+  test "should have destination_email before save" do
+    templated_email = create_valid_templated_email
+    templated_email.destination_email = nil
+
+    assert_not templated_email.save
+  end
+
+  test "should have message_template before save" do
+    templated_email = create_valid_templated_email
+    templated_email.message_template = nil
+
+    assert_not templated_email.save
+  end
+
+  private def create_valid_templated_email
+    result = TemplatedEmail.new
+    result.sender_name = "Sender Name"
+    result.destination_email = "test@example.com"
+    result.message_template = MessageTemplate.first
+
+    result
+  end
 end
