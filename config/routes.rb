3d72b27187c1d432de03a5b6a4b3536bbbc17492
Luke Dobinson <luke.a.dobinson@gmail.com>
2019-12-06 01:00:57 +0000

    Added about page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'templated_emails#index'
+
   resources :templated_emails
 
   resources :message_templates
+
+  get '/about', to: 'static_pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
